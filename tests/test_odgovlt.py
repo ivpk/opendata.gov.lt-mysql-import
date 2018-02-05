42ec3f06d681e45ee307e2c46a5f60d78207ec7a
JustNindze <ligitas.vinciunas1@gmail.com>
2018-02-05 14:41:24 +0200

    cache

--- tests/test_odgovlt.py
+++ tests/test_odgovlt.py
@@ -361,6 +361,13 @@ def test_OdgovltHarvester(app, db, mocker):
     sync = IvpkIrsSync(db)
     mocker.patch('odgovlt.IvpkIrsSync', return_value=sync)
     with requests_mock.Mocker() as m:
+        url = 'http://www.testas1.lt'
+        file1 = '/test1/test1/file1.pdf'
+        file2 = '/test2/test2/file2.doc'
+        href1 = '<a href="%s" target="_blank"></a>' % file1
+        href2 = '<a href="%s" target="_blank"></a>' % file2
+        page = href1 + href2
+        m.get(url, text=page, headers={'content-type': 'text/html'})
         results_by_guid = run_harvest(url='sqlite://', harvester=OdgovltHarvester())
     result = results_by_guid['1']
     assert result['state'] == 'COMPLETE'
